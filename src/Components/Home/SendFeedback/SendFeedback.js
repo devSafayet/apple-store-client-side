c17b5381230c9e602953d166243703031627b616
Safayet <mrishafayet003@gmail.com>
2022-05-19 05:05:20 +0600

    added customer feedback send

--- src/Components/Home/SendFeedback/SendFeedback.js
+++ src/Components/Home/SendFeedback/SendFeedback.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import { FaArrowRight } from "@react-icons/all-files/fa/FaArrowRight";
+import './SendFeedback.css';
 
 const SendFeedback = () => {
     return (
-        <div>
-
+        <div className='container send-feedback-Container p-4'>
+            <h4 className='text-center m-3'>Send Your Feedback</h4>
+            <form className='text-center'>
+                <textarea type="text" className='mb-2 ps-2 rounded w-50' name="text-area" id="" placeholder='Enter Your Description' required /><br></br>
+                <input type="text" className='mb-2 ps-2 rounded w-50' placeholder='Enter Your Email' required /><br></br>
+                <input type="text" className='mb-2 ps-2 rounded w-50' placeholder='Enter Your Name' required /> <br></br>
+                <input type="text" className='mb-2 ps-2 rounded w-50' name="" id="" placeholder='Send your Feedback Rate' required /><br></br> <br />
+                <button type="submit" className='btn btn-primary'>Send Your Feedback <FaArrowRight /></button>
+            </form>
         </div>
     );
 };
