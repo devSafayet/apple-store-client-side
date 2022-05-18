dbe8f6cee6db26992fc1dc464734bc4c5e4971c0
Safayet <mrishafayet003@gmail.com>
2022-05-18 14:51:26 +0600

    added home components

--- src/Components/Home/Home/Home.js
+++ src/Components/Home/Home/Home.js
@@ -1,9 +1,40 @@
 import React from 'react';
+import { Carousel } from 'react-bootstrap';
+import Items from '../Items/Items';
+import CustomerFeedback from '../CustomerFeedback/CustomerFeedback';
 
 const Home = () => {
     return (
         <div>
-            <h3>This is home section</h3>
+            <div className='mt-5'>
+                <Carousel className='w-100'>
+                    <Carousel.Item interval={1000}>
+                        <img
+                            className="d-block w-100"
+                            src="https://ibb.co/sQRNvFc"
+                            alt="First slide"
+                        />
+                    </Carousel.Item>
+                    <Carousel.Item interval={500}>
+                        <img
+                            className="d-block w-100"
+                            src="https://ibb.co/FxD9Zxy"
+                            alt="Second slide"
+                        />
+
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img
+                            className="d-block w-100"
+                            src="https://ibb.co/BVL52T3"
+                            alt="Third slide"
+                        />
+
+                    </Carousel.Item>
+                </Carousel>
+            </div>
+            <Items></Items>
+            <CustomerFeedback></CustomerFeedback>
         </div>
     );
 };
