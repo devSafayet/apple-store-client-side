952f75dd569d07e88c429a89ba5993a5e95bbb79
Safayet <mrishafayet003@gmail.com>
2022-05-19 03:16:52 +0600

    added notfound sections

--- src/Components/Shared/NotFound/NotFound.js
+++ src/Components/Shared/NotFound/NotFound.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import './NotFound.css';
 
 const NotFound = () => {
     return (
-        <div className='text-center text-danger'>
-            <h1>Soooory! This Page is Not Found! 404</h1>
+        <div className='text-center text-danger error-section mt-3'>
+            <h1>oooops! This Page is Not Found! 404</h1>
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRRdAQ8iTjm4itAUjvKdzcq3wFlIOnKeiUeVHn1y6Ij0J93AhR_nHSlDB6d0vDIn_ceKGM&usqp=CAU" alt="" />
         </div>
     );
 };
