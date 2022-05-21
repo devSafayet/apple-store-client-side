de4c32ec4f046c03e86e15da769b31be03f80517
Safayet <mrishafayet003@gmail.com>
2022-05-21 11:44:46 +0600

    added some component

--- src/Components/ManageProduct/ManageProduct.js
+++ src/Components/ManageProduct/ManageProduct.js
@@ -1,9 +1,15 @@
 import React from 'react';
 
-const ManageProduct = () => {
+const ManageProduct = ({ applestore, hendeldelete }) => {
+    // console.log(applestore);
     return (
         <div>
-
+            <tr className=''>
+                <th scope="row">{applestore.quantity}</th>
+                <td>{applestore.name}</td>
+                <td>{applestore.suppliername}</td>
+                <td className='text-primary pointer' onClick={() => hendeldelete(applestore._id)} role={"button"}>delete</td>
+            </tr>
         </div>
     );
 };
--- src/Components/ManageProducts/ManageProducts.js
+++ src/Components/ManageProducts/ManageProducts.js
@@ -1,9 +1,60 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import ManageProduct from '../ManageProduct/ManageProduct';
 
 const ManageProducts = () => {
-    return (
-        <div>
+    const [applestores, setApplestores] = useState([]);
 
+    useEffect(() => {
+        const url = "http://localhost:4000//products";
+
+        fetch(url)
+            .then(res => res.json())
+            .then(data => setApplestores(data))
+    }, [])
+
+    const hendeldelete = id => {
+
+        const procide = window.confirm("Are you Sure ? You want to Delete ?")
+        if (procide) {
+            console.log(id);
+            const url = `http://localhost:4000//products/${id}`
+            fetch(url, {
+                method: "DELETE"
+            })
+                .then(res => res.json())
+                .then(data => {
+                    console.log(data)
+                    if (data.deletedCount > 0) {
+                        console.log("deleted");
+                        const restproduct = applestores.filter(applestore => applestore._id !== id)
+                        setApplestores(restproduct)
+                    }
+                })
+        }
+
+    }
+    return (
+        <div style={{ height: "100%" }}>
+            <div className='container ps-3 h-100'>
+
+                <table class="table table-dark table-striped table-style manage-main">
+                    <thead>
+                        <tr>
+                            <th scope="col">Quantity</th>
+                            <th scope="col">Model</th>
+                            <th scope="col">Supplier</th>
+                            <th scope="col">Delete</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            applestores.map(applestore => <ManageProduct key={applestore._id} applestore={applestore} hendeldelete={hendeldelete}></ManageProduct>)
+                        }
+
+                    </tbody>
+                </table>
+
+            </div>
         </div>
     );
 };
